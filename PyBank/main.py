2ab0fdedd6129be6e535925ff55839f8a9b7691d
ginaleonardi <118944094+ginaleonardi@users.noreply.github.com>
2023-01-04 11:49:49 -0800

    final

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,11 +1,4 @@
-# Python script that analyzes the records to calculate each of the following:
-
-    #   The total number of months included in the dataset
-    #   The net total amount of "Profit/Losses" over the entire period
-    #   The changes in "Profit/Losses" over the entire period, and then the average of those changes
-    #   The greatest increase in profits (date and amount) over the entire period
-    #   The greatest decrease in profits (date and amount) over the entire period
-
+#import modules
 import os
 import csv
 
